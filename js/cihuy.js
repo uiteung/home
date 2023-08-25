91a59bd05b2bbde6e84384c3c79c12435b2d4950
christyuda <chrisyuda@ulbi.ac.id>
2023-08-25 12:49:00 +0700

    Modul Euis: mengganti encodeuri

--- js/cihuy.js
+++ js/cihuy.js
@@ -1,6 +1,6 @@
 import {
   CihuyId,
-  CihuyClickListenenr,
+  CihuyLinkClick,
 } from "https://c-craftjs.github.io/element/element.js";
 import { CihuyGetCookie } from "https://c-craftjs.github.io/cookies/cookies.js";
 import { CihuyDomReady } from "https://c-craftjs.github.io/table/table.js";
@@ -11,11 +11,8 @@ CihuyDomReady(() => {
 
   rtmlink.addEventListener("click", (event) => {
     event.preventDefault();
+    const newUrl = `https://rtm.ulbi.ac.id/index.php/auth?uuid=${token}`;
 
-    const newUrl = `https://rtm.ulbi.ac.id/index.php/auth?uuid=${encodeURIComponent(
-      token
-    )}`;
-
-    window.location.assign(newUrl);
+    window.location.assign(newUrl); // Mengarahkan pengguna ke URL baru
   });
 });
