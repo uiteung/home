85213b7de48f2f358e750779d4a09cc4c6a879b5
christyuda <chrisyuda@ulbi.ac.id>
2023-08-25 12:42:56 +0700

    Modul Euis: memperbaiki function href

--- js/cihuy.js
+++ js/cihuy.js
@@ -5,17 +5,17 @@ import {
 import { CihuyGetCookie } from "https://c-craftjs.github.io/cookies/cookies.js";
 import { CihuyDomReady } from "https://c-craftjs.github.io/table/table.js";
 
-function redirectRTM(token) {
-  const newUrl = `https://rtm.ulbi.ac.id/index.php/auth?uuid=${encodeURIComponent(
-    token
-  )}`;
-  window.location.assign(newUrl);
-}
-
 CihuyDomReady(() => {
+  const rtmlink = CihuyId("rtm");
   const token = CihuyGetCookie("login");
-  CihuyClickListenenr("rtm", (event) => {
+
+  rtmlink.addEventListener("click", (event) => {
     event.preventDefault();
-    redirectRTM(token);
+
+    const newUrl = `https://rtm.ulbi.ac.id/index.php/auth?uuid=${encodeURIComponent(
+      token
+    )}`;
+
+    window.location.assign(newUrl);
   });
 });
