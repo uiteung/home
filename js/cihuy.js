f006a8054cde1d61b6366feb9e0152d87b53908d
christyuda <chrisyuda@ulbi.ac.id>
2023-08-25 11:50:30 +0700

    Modul Euis: Menambahkan encodeURIcomponentToken

--- js/cihuy.js
+++ js/cihuy.js
@@ -5,9 +5,9 @@ import {
 import { CihuyGetCookie } from "https://c-craftjs.github.io/cookies/cookies.js";
 import { CihuyDomReady } from "https://c-craftjs.github.io/table/table.js";
 
-const token = CihuyGetCookie("login");
-
 CihuyDomReady(() => {
+  const token = CihuyGetCookie("login");
+
   const rtmlink = CihuyId("rtm");
   const uuid = token;
   //   console.log("Token:", token);
@@ -15,6 +15,9 @@ CihuyDomReady(() => {
   rtmlink.addEventListener("click", (event) => {
     // console.log("UUID:", uuid);
     // console.log(`URL: https://rtm.ulbi.ac.id/index.php/auth?uuid=${uuid}`);
-    CihuyLinkClick(event, `https://rtm.ulbi.ac.id/index.php/auth?uuid=${uuid}`);
+    CihuyLinkClick(
+      event,
+      `https://rtm.ulbi.ac.id/index.php/auth?uuid=${encodeURIComponent(token)}`
+    );
   });
 });
