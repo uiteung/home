209198617c5b510041dbd8e460819b7c78a707a5
christyuda <chrisyuda@ulbi.ac.id>
2023-08-25 15:54:06 +0700

    Modul EUIS : Memperbaiki Auto Login

--- js/cihuy.js
+++ js/cihuy.js
@@ -1,29 +1,24 @@
 import { CihuyId } from "https://c-craftjs.github.io/element/element.js";
 import { CihuyGetCookie } from "https://c-craftjs.github.io/cookies/cookies.js";
-import { CihuyDomReady } from "https://c-craftjs.github.io/table/table.js";
+document.addEventListener("DOMContentLoaded", function () {
+  // Dapatkan elemen dengan ID "rtm"
+  let rtmLink = document.getElementById("rtm");
 
-let rtmlink = CihuyId("rtm");
-let token = CihuyGetCookie("login");
+  // Dapatkan nilai cookie "login"
+  let token = CihuyGetCookie("login");
 
-// CihuyDomReady(() => {
-//   rtmlink.addEventListener("click", () => {
-//     // event.preventDefault();
-//     let newUrl = `https://rtm.ulbi.ac.id/index.php/auth?uuid=${token}`;
+  // Tambahkan event listener untuk mengarahkan saat elemen diklik
+  rtmLink.addEventListener("click", function (event) {
+    event.preventDefault(); // Mencegah perilaku default tautan
 
-//     window.location.replace(newUrl);
-//   });
-// });
-rtmLink.addEventListener("click", function (event) {
-  event.preventDefault(); // Mencegah perilaku default tautan
+    if (token) {
+      // Buat URL baru dengan menambahkan parameter uuid (gunakan nilai token)
+      let newUrl = `https://rtm.ulbi.ac.id/index.php/auth?uuid=${token}`;
 
-  if (token) {
-    // Buat URL baru dengan menambahkan parameter uuid (gunakan nilai token)
-    let newUrl = `https://rtm.ulbi.ac.id/index.php/auth?uuid=${token}`;
-
-    // Arahkan pengguna ke URL baru
-    window.location.assign = newUrl;
-    console.log(newUrl);
-  } else {
-    console.log("Token tidak ditemukan dalam cookie.");
-  }
+      // Arahkan pengguna ke URL baru
+      window.location.assign(newUrl);
+    } else {
+      console.log("Token tidak ditemukan dalam cookie.");
+    }
+  });
 });
