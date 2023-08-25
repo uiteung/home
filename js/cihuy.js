bd0553cab049fed5e671d0bd327d494d19e19880
christyuda <chrisyuda@ulbi.ac.id>
2023-08-25 14:15:08 +0700

    Modul EUIS: Mematikan Function click()

--- js/cihuy.js
+++ js/cihuy.js
@@ -1,26 +1,26 @@
-// import {
-//   CihuyId,
-//   CihuyLinkClick,
-// } from "https://c-craftjs.github.io/element/element.js";
-// import { CihuyGetCookie } from "https://c-craftjs.github.io/cookies/cookies.js";
-// import { CihuyDomReady } from "https://c-craftjs.github.io/table/table.js";
+import {
+  CihuyId,
+  CihuyLinkClick,
+} from "https://c-craftjs.github.io/element/element.js";
+import { CihuyGetCookie } from "https://c-craftjs.github.io/cookies/cookies.js";
+import { CihuyDomReady } from "https://c-craftjs.github.io/table/table.js";
 
-// CihuyDomReady(() => {
-//   const rtmlink = CihuyId("rtm");
-//   const token = CihuyGetCookie("login");
-
-//   rtmlink.addEventListener("click", (event) => {
-//     event.preventDefault();
-//     const newUrl = `https://rtm.ulbi.ac.id/index.php/auth?uuid=${token}`;
-
-//     window.location.assign(newUrl); // Mengarahkan pengguna ke URL baru
-//   });
-// });
-
-function rtmclick() {
+CihuyDomReady(() => {
+  const rtmlink = CihuyId("rtm");
   const token = CihuyGetCookie("login");
-  const newUrl = `https://rtm.ulbi.ac.id/index.php/auth?uuid=${encodeURIComponent(
-    token
-  )}`;
-  window.location.assign(newUrl);
-}
+
+  rtmlink.addEventListener("click", (event) => {
+    event.preventDefault();
+    const newUrl = `https://rtm.ulbi.ac.id/index.php/auth?uuid=${token}`;
+
+    window.location.assign(newUrl); // Mengarahkan pengguna ke URL baru
+  });
+});
+
+// function rtmclick() {
+//   const token = CihuyGetCookie("login");
+//   const newUrl = `https://rtm.ulbi.ac.id/index.php/auth?uuid=${encodeURIComponent(
+//     token
+//   )}`;
+//   window.location.assign(newUrl);
+// }
