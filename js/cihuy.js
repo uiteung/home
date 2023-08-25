52738fa1d23587b89fbfe8dcc7ad8127cb3ebe56
christyuda <chrisyuda@ulbi.ac.id>
2023-08-25 11:25:03 +0700

    Modul Euis: change GetCookies from link.js

--- js/cihuy.js
+++ js/cihuy.js
@@ -2,7 +2,7 @@ import {
   CihuyId,
   CihuyLinkClick,
 } from "https://c-craftjs.github.io/element/element.js";
-import { CihuyGetCookie } from "https://c-craftjs.github.io/link/link.js";
+import { CihuyGetCookie } from "https://c-craftjs.github.io/cookies/cookies.js";
 import { CihuyDomReady } from "https://c-craftjs.github.io/table/table.js";
 
 const token = CihuyGetCookie("login");
@@ -10,8 +10,11 @@ const token = CihuyGetCookie("login");
 CihuyDomReady(() => {
   const rtmlink = CihuyId("rtm");
   const uuid = token;
+  //   console.log("Token:", token);
 
   rtmlink.addEventListener("click", (event) => {
+    // console.log("UUID:", uuid);
+    // console.log(`URL: https://rtm.ulbi.ac.id/index.php/auth?uuid=${uuid}`);
     CihuyLinkClick(event, `https://rtm.ulbi.ac.id/index.php/auth?uuid=${uuid}`);
   });
 });
