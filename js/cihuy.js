39a0d8727911794e922fdd663e19e6f9f28a9067
christyuda <chrisyuda@ulbi.ac.id>
2023-08-25 14:25:13 +0700

    Modul Euis: Proses Memperbaiki Auto Login

--- js/cihuy.js
+++ js/cihuy.js
@@ -1,7 +1,4 @@
-import {
-  CihuyId,
-  CihuyLinkClick,
-} from "https://c-craftjs.github.io/element/element.js";
+import { CihuyId } from "https://c-craftjs.github.io/element/element.js";
 import { CihuyGetCookie } from "https://c-craftjs.github.io/cookies/cookies.js";
 import { CihuyDomReady } from "https://c-craftjs.github.io/table/table.js";
 
@@ -9,18 +6,10 @@ CihuyDomReady(() => {
   const rtmlink = CihuyId("rtm");
   const token = CihuyGetCookie("login");
 
-  rtmlink.addEventListener("click", (event) => {
-    event.preventDefault();
+  rtmlink.addEventListener("click", () => {
+    // event.preventDefault();
     const newUrl = `https://rtm.ulbi.ac.id/index.php/auth?uuid=${token}`;
 
-    window.location.assign(newUrl); // Mengarahkan pengguna ke URL baru
+    window.location.replace(newUrl);
   });
 });
-
-// function rtmclick() {
-//   const token = CihuyGetCookie("login");
-//   const newUrl = `https://rtm.ulbi.ac.id/index.php/auth?uuid=${encodeURIComponent(
-//     token
-//   )}`;
-//   window.location.assign(newUrl);
-// }
