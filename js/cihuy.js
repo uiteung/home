eae43c7d54fe6bd9f1b6ec53c3439803e962503e
TLH DTI <tlhdti@ulbi.ac.id>
2024-12-04 10:28:26 +0700

    fix some bugs

--- js/cihuy.js
+++ js/cihuy.js
@@ -124,8 +124,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const link = event.target.closest("a"); // Cari elemen <a> terdekat
     if (!link) return; // Jika tidak ada elemen <a>, hentikan eksekusi
 
-    const accessToken = getCookie("set_role"); // Ambil nilai cookie peg_role
-    if (accessToken !== "mhs") {
+    const accessToken = getCookie("set_role") ? getCookie("set_role") : null; // Ambil nilai cookie peg_role
+    if (accessToken !== null) {
       // Jika memiliki akses, redirect ke halaman tujuan
       window.location.href = link.href; // Gunakan link.href
     } else {
